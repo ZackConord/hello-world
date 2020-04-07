a1f5b2634860de7888171d1ebc1cc1a54f7b4093
thegoodbadugly <63135933+thegoodbadugly@users.noreply.github.com>
2020-04-07 17:31:44 -0400

    Session with Artie and Jeff

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello-world")
+	printEverythingbaby("Phase 1")
+	printEverythingbaby("Phase 2")
+	count(10)
+}
+
+func printEverythingbaby(Everythingbaby string) {
+	fmt.Println(Everythingbaby)
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println(fmt.Sprintf("%s is what you told me", input))
+}
+
+func count(num int) {
+	for i := 1; i <= num; i++ {
+		fmt.Println(i)
+	}
 }
