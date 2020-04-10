ec20e698acd4131250e29884afa5031f0aa48101
thegoodbadugly <63135933+thegoodbadugly@users.noreply.github.com>
2020-04-10 19:30:36 -0400

    anoter test

--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	printEverythingbaby("Phase 1")
-	printEverythingbaby("Phase 2")
-	count(10)
-}
-
 func printEverythingbaby(Everythingbaby string) {
 	fmt.Println(Everythingbaby)
 	var input string
 	fmt.Scanln(&input)
-	fmt.Println(fmt.Sprintf("%s is what you told me", input))
+	fmt.Println(fmt.Sprintf("%s is what you told me", "zack"))
 }
 
 func count(num int) {
 	for i := 1; i <= num; i++ {
 		fmt.Println(i)
 	}
+}
+func main() {
+	printEverythingbaby("Phase 1")
+	count(15)
+	printEverythingbaby("Phase 2")
+
 }
